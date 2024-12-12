fb379e5bfcf445821d5ac7fba99975df4836bd46
openmpy <suhwan@kakao.com>
2024-12-12 18:55:38 +0900

    feat: 문자열 리스트를 WorkDate 도메인으로 변환하는 기능 추가

---
+++ src/main/java/oncall/utils/StringParser.java
@@ -0,0 +1,17 @@
+package oncall.utils;
+
+import java.util.List;
+import oncall.domain.WorkDate;
+
+public class StringParser {
+
+    public static WorkDate parseWorkDate(List<String> strings) {
+        try {
+            int month = Integer.parseInt(strings.get(0));
+            String week = strings.get(1);
+            return new WorkDate(month, week);
+        } catch (RuntimeException e) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 입력 값입니다. 다시 입력해 주세요.");
+        }
+    }
+}

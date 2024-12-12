89a26ef8c03e153a10de33042939ac930c8621f3
openmpy <suhwan@kakao.com>
2024-12-12 21:27:42 +0900

    fix: 비상 근무 시스템 버그 수정

--- src/main/java/oncall/domain/Schedule.java
+++ src/main/java/oncall/domain/Schedule.java
@@ -12,8 +12,7 @@ public class Schedule {
     private final Deque<String> dayOffWorkersDeque = new ArrayDeque<>();
 
     private final WorkDate workDate;
-    private final WeekdayWorkers weekdayWorkers;
-    private final DayOffWorkers dayOffWorkers;
+
     private String lastNickname = null;
 
     public Schedule(WorkDate workDate, WeekdayWorkers weekdayWorkers, DayOffWorkers dayOffWorkers) {
@@ -21,47 +20,46 @@ public class Schedule {
         dayOffWorkersDeque.addAll(dayOffWorkers.getNicknames());
 
         this.workDate = workDate;
-        this.weekdayWorkers = weekdayWorkers;
-        this.dayOffWorkers = dayOffWorkers;
     }
 
     public void printSchedule() {
         int month = workDate.getMonth();
         int lastDay = Month.findLastDay(month);
-        int index = Week.findIndex(workDate.getWeek());
+        int startIndex = Week.findIndex(workDate.getWeek());
 
         for (int i = 0; i < lastDay; i++) {
-            int day = (index + i) % 7;
+            int day = i + 1;
+            int index = (startIndex + i) % 7;
 
-            String worker = getWorker(month, day);
-            worker = handleWorker(month, day, worker);
+            String worker = getWorker(month, day, index);
+            worker = handleWorker(month, day, index, worker);
+            lastNickname = worker;
 
-            String formatted = String.format("%d월 %d일 %s %s", month, i + 1, Week.findWeek(day), worker);
+            String formatted = String.format("%d월 %d일 %s %s", month, i + 1, Week.findWeek(index), worker);
             if (Holiday.isHoliday(month, i + 1)) {
-                formatted = String.format("%d월 %d일 %s(휴일) %s", month, i + 1, Week.findWeek(day), worker);
+                formatted = String.format("%d월 %d일 %s(휴일) %s", month, i + 1, Week.findWeek(index), worker);
             }
             System.out.println(formatted);
         }
     }
 
-    private String handleWorker(int month, int day, String worker) {
+    private String handleWorker(int month, int day, int index, String worker) {
         if (worker.equals(lastNickname)) {
-            if (!Holiday.isHoliday(month, day) && (day == 1 || day == 2 || day == 3 || day == 4 || day == 5)) {
+            if (!Holiday.isHoliday(month, day) &&
+                    (index == 1 || index == 2 || index == 3 || index == 4 || index == 5)) {
                 worker = weekdayWorkersDeque.pop();
-                weekdayWorkersDeque.add(lastNickname);
-                lastNickname = worker;
+                weekdayWorkersDeque.addFirst(lastNickname);
                 return worker;
             }
 
             worker = dayOffWorkersDeque.pop();
             dayOffWorkersDeque.addFirst(lastNickname);
-            lastNickname = worker;
         }
         return worker;
     }
 
-    private String getWorker(int month, int day) {
-        if (!Holiday.isHoliday(month, day) && (day == 1 || day == 2 || day == 3 || day == 4 || day == 5)) {
+    private String getWorker(int month, int day, int index) {
+        if (!Holiday.isHoliday(month, day) && (index == 1 || index == 2 || index == 3 || index == 4 || index == 5)) {
             return getWeekdayWorker();
         }
         return getDayOffWorker();

cd382730e67eaed7c20c204fe4af35d47108eede
openmpy <suhwan@kakao.com>
2024-12-12 19:00:27 +0900

    feat: 평일 비상 근무 사원 도메인 추가

---
+++ src/main/java/oncall/domain/WeekdayWorkers.java
@@ -0,0 +1,21 @@
+package oncall.domain;
+
+import java.util.List;
+
+public class WeekdayWorkers {
+
+    private final List<String> nicknames;
+
+    public WeekdayWorkers(List<String> nicknames) {
+        validateDuplicated(nicknames);
+
+        this.nicknames = nicknames;
+    }
+
+    private void validateDuplicated(List<String> nicknames) {
+        long distinctCount = nicknames.stream().distinct().count();
+        if (nicknames.size() != distinctCount) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 입력 값입니다. 다시 입력해 주세요.");
+        }
+    }
+}
